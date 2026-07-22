bb633aa42ad180a3e77ad6653b585af14ec8aaba
jui005 <juiud0503@gmail.com>
2026-07-22 10:54:17 +0530

    Authenticate User Api Created

---
+++ Backend/CRM/src/main/java/com/sunbeam/CRM/dto/LoginRequestDto.java
@@ -0,0 +1,13 @@
+package com.sunbeam.CRM.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
