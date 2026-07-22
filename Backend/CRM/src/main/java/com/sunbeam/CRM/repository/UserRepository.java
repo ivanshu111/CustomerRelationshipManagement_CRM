201455fd316b47724304b83624c002eb5229e553
Ivanshu pratap singh <ivanshuprataps@gmail.com>
2026-07-22 10:18:10 +0530

    User Register Api done

--- Backend/CRM/src/main/java/com/sunbeam/CRM/repository/UserRepository.java
+++ Backend/CRM/src/main/java/com/sunbeam/CRM/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Integer, Users> {
+public interface UserRepository extends JpaRepository<Users, Integer> {
     Optional<Users> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
