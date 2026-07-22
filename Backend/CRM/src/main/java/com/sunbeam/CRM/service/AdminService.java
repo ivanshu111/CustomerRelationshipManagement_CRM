ac358de95ef24755cd98f60e5f596810a74a90c5
subodhpatel3007-svg <subodhpatel3007@gmail.com>
2026-07-22 12:32:39 +0530

    added get all interactions api in admin controller

--- Backend/CRM/src/main/java/com/sunbeam/CRM/service/AdminService.java
+++ Backend/CRM/src/main/java/com/sunbeam/CRM/service/AdminService.java
@@ -1,11 +1,13 @@
 package com.sunbeam.CRM.service;
 
+import java.util.List;
+
 import com.sunbeam.CRM.dto.CustomerResponseDto;
 import com.sunbeam.CRM.dto.EmployeeResponseDto;
-
-import java.util.List;
+import com.sunbeam.CRM.dto.InteractionResponseDto;
 
 public interface AdminService {
     List<EmployeeResponseDto> getAllEmployees();
     List<CustomerResponseDto> getAllCustomersOfEmployee(Integer id);
+     List<InteractionResponseDto> getAllInteractions();
 }
