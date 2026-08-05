88d0fb35af1e9728a92662d5b875af56a6b150a3
jui005 <juiud0503@gmail.com>
2026-08-05 22:36:31 +0530

    Notification Controller and getMyNotification Implemented

---
+++ Backend/CRM/src/main/java/com/sunbeam/CRM/repository/NotificationRepository.java
@@ -0,0 +1,11 @@
+package com.sunbeam.CRM.repository;
+
+import com.sunbeam.CRM.entities.Notifications;
+import com.sunbeam.CRM.entities.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notifications, Integer> {
+    List<Notifications> findByRecipientOrderByCreatedAtDesc(Users recipient);
+}
---
+++ Backend/CRM/src/main/java/com/sunbeam/CRM/service/impl/SseNotificationService.java
@@ -0,0 +1,40 @@
+package com.sunbeam.CRM.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@RequiredArgsConstructor
+public class SseNotificationService {
+
+    private final Map<Integer, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    public SseEmitter createConnection(Integer userId) {
+        SseEmitter emitter = new SseEmitter(30 * 60 * 1000L);
+        emitters.put(userId, emitter);
+        emitter.onCompletion(() -> emitters.remove(userId));
+
+        emitter.onTimeout(() -> {emitter.complete();emitters.remove(userId);});
+
+        // Remove connection when an error occurs
+        emitter.onError((error) -> emitters.remove(userId));
+
+        try {
+            // Send initial event so browser knows connection is active
+            emitter.send(SseEmitter.event().name("connected").data("SSE connection established")
+            );
+        } catch (IOException e) {
+            emitters.remove(userId);
+            emitter.completeWithError(e);
+        }
+
+        return emitter;
+    }
+
+
+}
