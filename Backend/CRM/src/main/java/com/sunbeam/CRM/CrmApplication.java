8ac26ba4923c35fff74e91ad6530dd0ae705d327
jui005 <juiud0503@gmail.com>
2026-08-05 21:26:18 +0530

    Notifications Entity added

--- Backend/CRM/src/main/java/com/sunbeam/CRM/CrmApplication.java
+++ Backend/CRM/src/main/java/com/sunbeam/CRM/CrmApplication.java
@@ -1,13 +1,41 @@
 package com.sunbeam.CRM;
 
+import com.sunbeam.CRM.entities.Role;
+import com.sunbeam.CRM.entities.Users;
+import com.sunbeam.CRM.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CrmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrmApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(CrmApplication.class, args);}
+
+	@Bean
+	CommandLineRunner init(UserRepository userRepository,
+	                       PasswordEncoder passwordEncoder) {
+
+		return args -> {
+
+			if (userRepository.findByEmail("admin@gmail.com").isEmpty()) {
+
+				Users admin = new Users();
+
+				admin.setName("System Admin");          // Required
+				admin.setEmail("admin@gmail.com");
+				admin.setPassword(passwordEncoder.encode("admin123"));
+				admin.setRole(Role.ADMIN);
+
+				userRepository.save(admin);
+
+				System.out.println("Default admin created.");
+			}
+
+		};
+
 	}
 
 }
---
+++ Backend/CRM/src/main/java/com/sunbeam/CRM/entities/Notifications.java
@@ -0,0 +1,41 @@
+package com.sunbeam.CRM.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notifications {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipient_id", nullable = false)
+    private Users recipient;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NotificationType type;
+
+    @Column(nullable = false)
+    private boolean isRead = false;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
