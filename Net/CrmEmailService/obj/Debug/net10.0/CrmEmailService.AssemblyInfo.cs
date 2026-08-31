45e51b77513bad966dda1bdb736a46ce46e69c4d
Ivanshu pratap singh <ivanshuprataps@gmail.com>
2026-08-31 15:58:21 +0530

    Add Hibernate dialect configuration for MySQL in application.properties

--- Net/CrmEmailService/obj/Debug/net10.0/CrmEmailService.AssemblyInfo.cs
+++ Net/CrmEmailService/obj/Debug/net10.0/CrmEmailService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrmEmailService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4f14e979c204cb7ae313695125d53baf78a759")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5f1dd6a37c95178f5c15b74282d12a69e879972")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrmEmailService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrmEmailService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
