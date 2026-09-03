39b321cf8614b5c46ebc457260dc933bfcd2ed4f
Ivanshu pratap singh <ivanshuprataps@gmail.com>
2026-09-03 11:05:31 +0530

    Update assembly version and add Vercel configuration for frontend routing

--- Net/CrmEmailService/obj/Debug/net10.0/CrmEmailService.AssemblyInfo.cs
+++ Net/CrmEmailService/obj/Debug/net10.0/CrmEmailService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrmEmailService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5f1dd6a37c95178f5c15b74282d12a69e879972")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41781e1f6fc12d2b95bfa5c631665bb5e063884d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrmEmailService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrmEmailService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
